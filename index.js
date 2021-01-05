e62df4e7ce08d7187d8a8f429c26c61bac4ebb29
edmundo papi <edmundopierre@yahoo.com>
2021-01-05 18:12:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ let createEmployeeRecord = function(arr){
     timeOutEvents: []
   }
 }
+
+let createEmployeeRecords =function(arOf){
+return  arOf.map((record)=> createEmployeeRecord(record))
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
